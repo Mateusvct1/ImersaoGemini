173d2a09f3aad6c46a3c73de12b1c8335293c416
Mateus Victor <mateusvictorct@gmail.com>
2024-09-04 20:12:39 -0300

    criado o dados.js e atualizado o app.js e o index.html

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("hello word")
+let dados = [
+    {
+        
+            titulo: "Rayssa Leal",
+            descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
+            link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+        
+    },
+    {
+        titulo: "Aline Silva",
+        descricao: "Aline Silva é uma judoca brasileira, campeã olímpica nos Jogos Olímpicos de 2016. Sua história de superação e determinação a tornou um ícone do esporte nacional.",
+        link: "https://pt.wikipedia.org/wiki/Aline_Silva_(judoca)"
+    },
+    {
+        
+        titulo: "Letícia Bufoni",
+        descricao: "Letícia Bufoni é uma skatista profissional brasileira, considerada uma das melhores do mundo em sua modalidade. Conhecida por sua técnica e criatividade, ela possui uma longa e bem-sucedida carreira.",
+        link: "https://pt.wikipedia.org/wiki/Letícia_Bufoni"
+    }
+]
